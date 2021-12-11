492040c9c0908867aa22b413836636a6433295df
FeyzaBiber <feyzabiber@gmail.com>
2021-12-11 23:08:10 +0300

    ci(app): add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,39 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <v-app>
+      <header-menu />
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import HeaderMenu from "./components/HeaderMenu.vue";
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  components: { HeaderMenu },
+  name: "App",
+  data() {
+    return {};
   },
-
-  data: () => ({
-    //
-  }),
+  methods: {
+    addChar(e) {
+      this.title = e;
+    },
+  },
 };
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+  display: flex;
+  flex-flow: column;
+}
+</style>
