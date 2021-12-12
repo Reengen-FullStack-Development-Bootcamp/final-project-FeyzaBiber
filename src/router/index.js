e581abef8ae99a62fc5c2e2cc40fadbe3403b1a4
FeyzaBiber <feyzabiber@gmail.com>
2021-12-12 13:59:33 +0300

    feat(router): add router indexjs

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+import UserSettings from "@/views/UserSettings.vue";
+import Dashboard from "@/views/Dashboard.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/usersettings",
+    name: "UserSettings",
+    component: UserSettings,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
